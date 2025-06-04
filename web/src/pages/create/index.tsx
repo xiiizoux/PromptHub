bae400d57ca2a2ff0ba7d82232f7a6a61f6eaeb8
zou <zouguojunx@gmail.com>
2025-06-04 00:40:11 +0000

    修复：将新提示的默认隐私设置为私密

--- web/src/pages/create/index.tsx
+++ web/src/pages/create/index.tsx
@@ -418,7 +418,7 @@ function CreatePromptPage() {
                       type="checkbox" 
                       className="sr-only peer" 
                       {...register('is_public')} 
-                      defaultChecked={true}
+                      defaultChecked={false}
                     />
                     <div className="w-11 h-6 bg-gray-700 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-neon-cyan rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-neon-cyan"></div>
                   </label>
